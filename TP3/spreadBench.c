aea137916396660ea086ac1767f8b7c93c5f12c1
Luis Santiago Re <lsantire@hotmail.com>
2019-11-16 18:26:15 -0800

    Codigo TP3 funcionando

--- TP3/spreadBench.c
+++ TP3/spreadBench.c
@@ -2,97 +2,125 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define GROUP_NAME "vsoa_tp3"
+#define MAX_MESSAGE_LENGTH 10000
 
-const char SPREAD_NAME[5] = "4803";
-
+char SPREAD_NAME[80] = "4803@";
 
 int main( int argc, char *argv[] )
 {
-	if(argc < 5)
+
+	if(argc < 5 || argc > 6)
 	{
-		printf("Incorrect number of arguments.\nExpected execution: spreadBench <NODE ID> <MAX NODES> <MSG COUNT> <MSG LEN>\n");
+		printf("Incorrect number of arguments.\nExpected execution: spreadBench <NODE ID> <MAX NODES> <MSG COUNT> <MSG LEN> [<DAEMON IP>]\n");
+		return 0;
 	}
-	int membersExpected = atoi(argv[2]), membersConnected = 0, *numGroups, *mismatch, messagesReceived;
-	int nodeId = atoi(argv[1]), cantMessages = atoi(argv[3]), messageLength = atoi(argv[4]);
-	char *GROUP_NAME, *message, *receivedMessage, sender[128], groups[1][32], auxStr[2]="";
-	mailbox *mbox;
-	service *service_type;
-	int16 *mess_type;
+	
+	if(argc==5) strcat(SPREAD_NAME, "localhost");
+	else strcat(SPREAD_NAME, argv[5]);
 
-	if(messageLength < strlen("EVERYONE_IN")) messageLength = strlen("EVERYONE_IN");
+	int membersExpected = atoi(argv[2]);
+	int membersConnected = 0;
+	int numGroups;
+	int mismatch;
+	int messagesReceived;
+	int nodeId = atoi(argv[1]);
+	int cantMessages = atoi(argv[3]);
+	int messageLength = atoi(argv[4]);
+	int spResult;
+	char privateGroupName[MAX_GROUP_NAME];
+	char sender[MAX_GROUP_NAME];
+	char groups[1][32];
+	char auxStr[2];
+	char  message[MAX_MESSAGE_LENGTH];
+	char receivedMessage[MAX_MESSAGE_LENGTH]; 
+	mailbox mbox;
+	service service_type;
+	int16 mess_type;
+	bool everyoneIn = 0;
 
-	if (SP_connect(SPREAD_NAME, NULL, 1, 1, mbox, GROUP_NAME) != ACCEPT_SESSION)
+	if(messageLength >= MAX_MESSAGE_LENGTH)
+	{
+		printf("ERROR! Message length must be less than %d\n", MAX_MESSAGE_LENGTH);
+		return 0;
+	}
+
+	if (SP_connect(SPREAD_NAME, NULL, 0, 1, &mbox, privateGroupName) != ACCEPT_SESSION)
 	{
 		printf("ERROR! connection not accepted\n");
 		return 0; 
 	}
 
-	if (SP_join (*mbox,GROUP_NAME) != 0)
+	if (SP_join (mbox,GROUP_NAME) != 0)
 	{
 		printf("ERROR while trying to join the group %s\n", GROUP_NAME);
 		return 0; 
 	}
 
-	while(membersConnected < membersExpected)
+	while(!everyoneIn)
 	{
 		printf("Waiting for message...\n");
-		if(SP_receive(*mbox, service_type, sender, 1, numGroups, groups, mess_type, mismatch, messageLength,receivedMessage)<0)
+		spResult = SP_receive(mbox, &service_type, sender, 1, &numGroups, groups, &mess_type, &mismatch, sizeof(receivedMessage), receivedMessage);
+		if(spResult<0)
 		{
-	  		printf("ERROR while receiving a message\n");
-	  		SP_leave (*mbox,GROUP_NAME);
+	  		printf("ERROR while receiving a message: %d\n", spResult);
+	  		SP_leave (mbox,GROUP_NAME);
 			return 0;
 		}
 		printf("Message received: ");
-		if(Is_caused_join_mess(*service_type)!=0)
+		if(Is_caused_join_mess(service_type)!=0)
 		{
 			printf("join\n");
 			membersConnected++;
-			if(membersConnected == membersExpected)
+			if(membersConnected == membersExpected && !everyoneIn)
 			{
 				strcpy(message, "EVERYONE_IN");
 				printf("Sending %s multicast\n", message);
-				if(SP_multicast(*mbox, RELIABLE_MESS, GROUP_NAME, 0, strlen(message), message) < 0)
+				if(SP_multicast(mbox, RELIABLE_MESS, GROUP_NAME, 0, strlen(message), message) < 0)
 				{
 					printf("ERROR while sending message: %s\n", message);
-					SP_leave (*mbox,GROUP_NAME);
+					SP_leave (mbox,GROUP_NAME);
 					return 0;
 				}
 			}
 		}
-		else if(Is_regular_mess(*service_type)!=0 && strcmp(receivedMessage, "EVERYONE_IN") == 0)
+		else if(strcmp(receivedMessage, "EVERYONE_IN") == 0)
 		{
 			printf("%s\n", receivedMessage);
-			membersConnected = membersExpected;
+			everyoneIn = 1;
 		}
-		else if(Is_caused_leave_mess(*service_type)!=0)
+		else if(Is_caused_leave_mess(service_type)!=0)
 		{
 			printf("ERROR. Leave message received, aborting...\n");
-			SP_leave (*mbox,GROUP_NAME);
+			SP_leave (mbox,GROUP_NAME);
 			return 0;
 		}
 		else
 		{
 			printf("\nERROR. Unexpected message received: %s\n", receivedMessage);
-			SP_leave (*mbox,GROUP_NAME);
+			SP_leave (mbox,GROUP_NAME);
 			return 0;
 		}
 	}
 
 	for (int i = 0; i <  cantMessages; ++i)
 	{
-		strcpy(message, "");	
-		auxStr[1]='\0';
+		strcpy(message, "");
+		auxStr[1] = '\0';
+		message[messageLength] = '\0';
+		receivedMessage[messageLength] = '\0';
 		for (int j = 0; j < messageLength; ++j)
 		{
-			auxStr[0] = (char)((33+i)%(126-33));
+			auxStr[0] = (char)(33+i%(126-33));
 			strcat(message,auxStr);
 		}
 
+
 		printf("Sending message: %s\n", message);
-		if (SP_multicast(*mbox, RELIABLE_MESS, GROUP_NAME, 0, strlen(message), message) < 0)
+		if (SP_multicast(mbox, RELIABLE_MESS, GROUP_NAME, 0, strlen(message), message) < 0)
 		{
 			printf("ERROR while sending message\n");
-			SP_leave (*mbox,GROUP_NAME);
+			SP_leave (mbox,GROUP_NAME);
 			return 0; 
 		}
 
@@ -101,22 +129,23 @@ int main( int argc, char *argv[] )
 		while(messagesReceived < membersExpected)
 		{
 			printf("Waiting for message...\n");
-			if (SP_receive(*mbox, service_type, sender, 1, numGroups, groups, mess_type, mismatch, messageLength, receivedMessage) < 0)
+			strcpy(receivedMessage, "");
+			if (SP_receive(mbox, &service_type, sender, 1, &numGroups, groups, &mess_type, &mismatch, sizeof(receivedMessage), receivedMessage) < 0)
 			{
 			  	printf("ERROR while receiving message\n");
-			  	SP_leave (*mbox,GROUP_NAME);
+			  	SP_leave (mbox,GROUP_NAME);
 				return 0;
 			}
-			if(Is_caused_leave_mess(*service_type)!=0)
+			if(Is_caused_leave_mess(service_type)!=0)
 			{
 				printf("ERROR. Leave message received, aborting...\n");
-				SP_leave (*mbox,GROUP_NAME);
+				SP_leave (mbox,GROUP_NAME);
 				return 0;
 			}
 			if(strcmp(message, receivedMessage) != 0)
 			{
-				printf("ERROR. Unexpected message received: %s\n, aborting...\n", receivedMessage);
-				SP_leave(*mbox,GROUP_NAME);
+				printf("ERROR. Expected %s and received %s, aborting...\n", message, receivedMessage);
+				SP_leave(mbox,GROUP_NAME);
 				return 0;
 			}
 			printf("Message received: %s\n", receivedMessage);
@@ -124,7 +153,7 @@ int main( int argc, char *argv[] )
 		}
 	}
 	
-	SP_leave(*mbox,GROUP_NAME);
+	SP_leave(mbox,GROUP_NAME);
 	
 	return 0;
 }
